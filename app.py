8e9b96defa7d569e0bdcec9b4e6b4a85d4a325dd
Thao Nguyen <thao.nnp@gmx.de>
2024-07-04 09:03:19 +0200

    - Added Host and Port in app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import pickle
 import numpy as np
 
 # Load the models
-with open('linear_model.pkl', 'rb') as file:
-    linear_model = pickle.load(file)
+#with open('linear_model.pkl', 'rb') as file:
+#    linear_model = pickle.load(file)
 
-with open('decision_tree_model.pkl', 'rb') as file:
-    decision_tree_model = pickle.load(file)
+#with open('decision_tree_model.pkl', 'rb') as file:
+    #decision_tree_model = pickle.load(file)
 
 with open('random_forest_model.pkl', 'rb') as file:
     random_forest_model = pickle.load(file)
@@ -24,11 +24,17 @@ def predict():
     # Prepare the input data for the model
     input_data = np.array([[year, month]])
 
-    # Choose the model for prediction (you can change this to use a different model)
+    # Choose the best model for prediction
     prediction = random_forest_model.predict(input_data)[0]
 
     return jsonify({'prediction': prediction})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = '0.0.0.0'
+    port = 5000
+
+    print(f"Starting Flask app on {host}:{port}")
+    app.run(debug=True, host=host, port=port)
+
+
